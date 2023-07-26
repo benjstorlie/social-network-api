cd4fde7f40f6b1f5c1ed700524c18f11737d8600
bjs <127002178+benstorlie@users.noreply.github.com>
2023-07-26 12:55:32 -0700

    initial commit with some model setup

---
+++ models/Thought.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose');
+
+// dayjs for formatting dates
+const dayjs = require('dayjs');
+
+function timestamp(date) {
+  return dayjs(date).format("[at] h:mm a [on] D MMM YYYY");
+}
+
+// Schemas define the shape of the documents within the collection.
+const reactionSchema = new mongoose.Schema(
+    {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    username: {
+      type: String, 
+      required: true 
+    },
+    reationBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    createdAt: { 
+      type: Date, 
+      default: Date.now, 
+      get: timestamp
+    },
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+const thoughtSchema = new mongoose.Schema(
+    {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    createdAt: { 
+      type: Date, 
+      default: Date.now,
+      get: timestamp, 
+    },
+    // This user will have its own array of Thoughts, containing this thought's _id.
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema
+  .virtual('reactionCount')
+  // Getter
+  .get(function() {
+    return this.reactions.length
+  });
+
+
+
+
+// Initialize Thought model
+const Thought = mongoose.model('thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+// Schemas define the shape of the documents within the collection.
+const userSchema = new mongoose.Schema(
+  {
+    username: { 
+      type: String, 
+      required: true,
+      unique: true, 
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      // Match email regex [Pattern created by 'VeteranKamikaze' from https://regexr.com/3e48o]
+      match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+    },
+    // Array of _id values referencing the Thought model
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thought',
+      },
+    ],
+    // Array of _id values referencing the User model (self-reference)
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function() {
+    return this.friends.length
+  });
+
+
+// Initialize User model
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
